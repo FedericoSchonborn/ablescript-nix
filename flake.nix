d299613c19479967004595d2c36ecc275be4b156
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-11-08 23:13:36 -0300

    Drop flake-utils
    Fixes #2
    
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,12 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
 
     ablescript0_5_2-src = {
       # v0.5.2
@@ -42,11 +36,15 @@
       url = "git+https://git.ablecorp.us/AbleScript/able-script?rev=8ef7118dc1270386d5455e4d407a272a0c7b0724";
       flake = false;
     };
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
   outputs = {
     nixpkgs,
-    flake-utils,
     ablescript0_5_2-src,
     ablescript0_5-src,
     ablescript0_4-src,
@@ -54,67 +52,73 @@
     ablescript0_2-src,
     ablescript-unstable-src,
     ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
+  }: let
+    # TODO: macOS support?
+    forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux"];
+  in rec {
+    packages = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
     in rec {
-      packages = rec {
-        ablescript0_5_2 = pkgs.callPackage ./packages/ablescript.nix {
-          version = "0.5.2";
-          src = ablescript0_5_2-src;
-          cargoSha256 = "2tBb8FdXknTbgFrjfO5ZM3O9OAnVuLVY/YcL8y/W7nA=";
-        };
-
-        ablescript0_5 = pkgs.callPackage ./packages/ablescript.nix {
-          version = "0.5.0";
-          src = ablescript0_5-src;
-          cargoSha256 = "Sua2KUv+ucq+80ypOw+T/TkC5waoSTxjxEJtHR4nxX0=";
-        };
-
-        ablescript0_4 = pkgs.callPackage ./packages/ablescript.nix {
-          version = "0.4.0";
-          src = ablescript0_4-src;
-          cargoSha256 = "MBPiqTE6H4qbidtTnSIBtYoMHb6mOG9hW0/LBLJ+N3M=";
-        };
-
-        ablescript0_3 = pkgs.callPackage ./packages/ablescript.nix {
-          version = "0.3.0";
-          src = ablescript0_3-src;
-          cargoSha256 = "/M7VuFhvR34QNo5a1lcwuYXm8QDQG0eYkoPYaLpWkZQ=";
-        };
-
-        ablescript0_2 = pkgs.callPackage ./packages/ablescript.nix {
-          version = "0.2.0";
-          src = ablescript0_2-src;
-          cargoSha256 = "Q+uR+0H/YruwWVfd/nMwicsfes7hVFrRVpmZuBd2gS4=";
-        };
-
-        ablescript-unstable = pkgs.callPackage ./packages/ablescript.nix {
-          stable = false;
-          version = "unstable-2022-09-20";
-          src = ablescript-unstable-src;
-          cargoSha256 = "wa1D+C3d+pFhUo8wexCbfv5A/O3QyXaE6xBRH/JclWg=";
-        };
-
-        ablescript = ablescript0_5_2;
-        default = ablescript;
+      ablescript0_5_2 = pkgs.callPackage ./packages/ablescript.nix {
+        version = "0.5.2";
+        src = ablescript0_5_2-src;
+        cargoSha256 = "2tBb8FdXknTbgFrjfO5ZM3O9OAnVuLVY/YcL8y/W7nA=";
       };
 
-      overlays = rec {
-        ablescript = self: super: {
-          ablescript = packages.${super.system}.ablescript;
-          ablescript-unstable = packages.${super.system}.ablescript-unstable;
-        };
-
-        default = ablescript;
+      ablescript0_5 = pkgs.callPackage ./packages/ablescript.nix {
+        version = "0.5.0";
+        src = ablescript0_5-src;
+        cargoSha256 = "Sua2KUv+ucq+80ypOw+T/TkC5waoSTxjxEJtHR4nxX0=";
       };
 
-      devShells.default = pkgs.mkShell {
+      ablescript0_4 = pkgs.callPackage ./packages/ablescript.nix {
+        version = "0.4.0";
+        src = ablescript0_4-src;
+        cargoSha256 = "MBPiqTE6H4qbidtTnSIBtYoMHb6mOG9hW0/LBLJ+N3M=";
+      };
+
+      ablescript0_3 = pkgs.callPackage ./packages/ablescript.nix {
+        version = "0.3.0";
+        src = ablescript0_3-src;
+        cargoSha256 = "/M7VuFhvR34QNo5a1lcwuYXm8QDQG0eYkoPYaLpWkZQ=";
+      };
+
+      ablescript0_2 = pkgs.callPackage ./packages/ablescript.nix {
+        version = "0.2.0";
+        src = ablescript0_2-src;
+        cargoSha256 = "Q+uR+0H/YruwWVfd/nMwicsfes7hVFrRVpmZuBd2gS4=";
+      };
+
+      ablescript-unstable = pkgs.callPackage ./packages/ablescript.nix {
+        stable = false;
+        version = "unstable-2022-09-20";
+        src = ablescript-unstable-src;
+        cargoSha256 = "wa1D+C3d+pFhUo8wexCbfv5A/O3QyXaE6xBRH/JclWg=";
+      };
+
+      ablescript = ablescript0_5_2;
+      default = ablescript;
+    });
+
+    overlays = rec {
+      ablescript = self: super: {
+        ablescript = packages.${super.system}.ablescript;
+        ablescript-unstable = packages.${super.system}.ablescript-unstable;
+      };
+
+      default = ablescript;
+    };
+
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
           just
         ];
       };
-
-      formatter = pkgs.alejandra;
     });
+
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+  };
 }
